c077272de8cdd8b53c34b232c6ffcabd835e04ab
haofang <fangh9416@gmail.com>
2019-09-17 12:40:17 -0400

    CRUD completed

--- Insta/models.py
+++ Insta/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from django.urls import reverse
+
 from imagekit.models import ProcessedImageField
 
 # Create your models here.
@@ -11,3 +15,16 @@ class Post(models.Model):
         blank = True,
         null = True
     )
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
+
+
+class InstaUser(AbstractUser):
+    profile_pic = ProcessedImageField(
+        upload_to='static/images/profiles',
+        format='JPEG',
+        options={'quality': 100},
+        null=True,
+        blank=True,
+        )
--- Insta/views.py
+++ Insta/views.py
@@ -1,4 +1,43 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from Insta.forms import CustomUserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
+
+from Insta.models import Post
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+    login_url = 'login'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("posts")
+
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    # form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
